6a40119012faa6f69999a85ad4f5a35ec670a647
AdrielCosta <adrielcosta220@gmail.com>
2022-11-01 17:31:28 -0300

    boletimgear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TelainicioComponent } from './telainicio/telainicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelainicioComponent
   ],
   imports: [
     BrowserModule,
